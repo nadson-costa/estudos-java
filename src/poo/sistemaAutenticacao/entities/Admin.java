e3e208adefe53d61acd9261ce46db0f44410b590
Nadson Costa <146302133+nadson-costa@users.noreply.github.com>
2026-01-07 23:29:03 -0300

    feat: Implementação da classe Admin e getter de 'departamento'

--- src/poo/sistemaAutenticacao/entities/Admin.java
+++ src/poo/sistemaAutenticacao/entities/Admin.java
@@ -1,60 +1,50 @@
 package poo.sistemaAutenticacao.entities;
 
-import poo.sistemaAutenticacao.interfaces.*;
+import poo.sistemaAutenticacao.interfaces.Administravel;
+import poo.sistemaAutenticacao.interfaces.Autenticavel;
 
 import java.util.ArrayList;
 
-public class Gerente extends Usuario implements Autenticavel, Administravel, Auditavel {
-    private String departamento;
-    private ArrayList<String> historico = new ArrayList<>();
+public class Admin extends Usuario implements Autenticavel, Administravel {
+    private int nivel;
     private ArrayList<Usuario> usuariosGerenciados = new ArrayList<>();
 
-    public Gerente(String nome, String email, String senha, String departamento){
+    public Admin(String nome, String email, String senha, int nivel){
         super(nome, email, senha);
-        this.departamento = departamento;
+        this.nivel = nivel;
     }
 
-    @Override
     public boolean autenticar(String senha){
         return senha.equals(this.senha);
     }
 
-    @Override
     public void login(){
         if(logado){
-            System.out.println("-> Gerente já está logado!");
+            System.out.println("-> Administrador já está logado!");
             return;
         }
         setLogado(true);
-        registrarAcao(">> Gerente fez o login com sucesso!");
         System.out.println("Login realizado com sucesso!");
     }
 
-    @Override
     public void logout(){
         if(!logado){
-            System.out.println("-> Gerente não está logado");
+            System.out.println("-> Administrador não está logado");
             return;
         }
-
         this.logado = false;
-        registrarAcao(">> Logout realizado");
         System.out.println("-> Logout realizado com sucesso!");
     }
 
-    @Override
     public void adicionarUsuario(Usuario usuario){
         usuariosGerenciados.add(usuario);
-        registrarAcao(">> Usuário adicionado com sucesso pelo Gerente!");
         System.out.println("-> Usuário adicionado com sucesso!");
     }
 
-    @Override
     public void removerUsuario(String nome){
         for(Usuario usuario : usuariosGerenciados){
             if(usuario.getNome().equalsIgnoreCase(nome)){
                 usuariosGerenciados.remove(usuario);
-                registrarAcao("Removeu usuario: " + nome);
                 System.out.println("✓ Usuario removido com sucesso!");
                 return;
             }
@@ -62,7 +52,6 @@ public class Gerente extends Usuario implements Autenticavel, Administravel, Aud
         System.out.println("Usuario nao encontrado!");
     }
 
-    @Override
     public void listarUsuarios(){
         System.out.println("=== LISTA DE USUÁRIOS ===");
         if(usuariosGerenciados.isEmpty()){
@@ -73,28 +62,5 @@ public class Gerente extends Usuario implements Autenticavel, Administravel, Aud
         }
         System.out.println("====================");
     }
-
-    @Override
-    public void registrarAcao(String acao){
-        historico.add(acao);
-    }
-
-    @Override
-    public void exibirHistorico(){
-        if(historico.isEmpty()){
-            System.out.println("-> Nenhuma ação realizada");
-            return;
-        }
-        System.out.println("=== HISTÓRICO DE AÇÕES ===");
-        for(String acao : historico){
-            System.out.println("- " + acao);
-        }
-        System.out.println("========================");
-    }
-
-    public String getDepartamento(){
-        return departamento;
-    }
-
 
 }
